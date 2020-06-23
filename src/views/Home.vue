afced1bab982ae08e68c57dcd7660a41557ebe91
chaiguoqing <chaigq@jovision.com>
2020-06-23 17:50:14 +0800

    add range

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <slider />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import slider from "./slider.vue";
+
+export default {
+  name: "Home",
+  components: {
+    slider
+  }
+};
+</script>
---
+++ src/views/slider.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="content">
+    <range :min="10" :max="100" v-model="sliderValue" :isShowTip="true"></range>
+  </div>
+</template>
+<script>
+import range from "@/components/range.vue";
+export default {
+  data() {
+    return {
+      sliderValue: 10
+    };
+  },
+  components: {
+    range
+  },
+  created() {},
+  mounted() {},
+  methods: {}
+};
+</script>
+<style scoped>
+.content {
+  width: 500px;
+  margin: 50px auto;
+}
+</style>
