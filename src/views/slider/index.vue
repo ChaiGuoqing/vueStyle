bb1c5c0ffe24f9b540e87d2cb0aac66903cdf5f8
chaiguoqing <chaigq@jovision.com>
2020-06-23 19:52:12 +0800

    modify style

--- src/views/slider/index.vue
+++ src/views/slider/index.vue
@@ -1,6 +1,37 @@
 <template>
   <div class="content">
-    <range v-model="sliderValue" :min="10" :max="100" :is-show-tip="true" />
+    <div class="btn">
+      <el-row>
+        <el-button type="primary" @click="sliderValue=20">sliderValue = 20</el-button>
+        <el-button type="success" @click="sliderValue=40">sliderValue = 40</el-button>
+        <el-button type="info" @click="sliderValue=60">sliderValue = 60</el-button>
+        <el-button type="warning" @click="sliderValue=80">sliderValue = 80</el-button>
+        <el-button type="danger" @click="sliderValue=100">sliderValue = 100</el-button>
+      </el-row>
+    </div>
+    <div>
+      <el-row :gutter="20">
+        <el-col :span="16">
+          <div>
+            <range v-model="sliderValue" :min="10" :max="100" :is-show-tip="showTip" />
+          </div>
+        </el-col>
+        <el-col :span="2">
+          <div>
+            {{ sliderValue }} %
+          </div>
+        </el-col>
+        <el-col :span="4">
+          <div>
+            <el-switch
+              v-model="showTip"
+              active-text="是否显示tip"
+            />
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+
   </div>
 </template>
 <script>
@@ -11,7 +42,8 @@ export default {
   },
   data() {
     return {
-      sliderValue: 10
+      sliderValue: 10,
+      showTip: true
     }
   },
   created() {},
@@ -21,6 +53,9 @@ export default {
 </script>
 <style scoped>
 .content {
-  margin: 50px 200px;
+  margin: 50px 60px;
+}
+.btn{
+  margin: 60px 0;
 }
 </style>
