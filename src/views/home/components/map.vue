70f9788ae6a0b0713d8181e9d6655e19aed698da
chaiguoqing <chaigq@jovision.com>
2020-06-30 18:40:54 +0800

    add echart map

---
+++ src/views/home/components/map.vue
@@ -0,0 +1,48 @@
+<!--  -->
+<template>
+  <div class="">
+    <div ref="myCharts" class="chartBox_d" />
+  </div>
+</template>
+
+<script>
+import echarts from 'echarts'
+import option from '../js/mapOption'
+import 'echarts/map/js/province/guangdong.js'
+// import 'echarts/map/js/china.js' // 引入中国地图数据
+require('echarts/theme/macarons')
+export default {
+// import引入的组件需要注入到对象中才能使用
+  components: {},
+  data() {
+    return {
+
+    }
+  },
+  // 生命周期 - 创建完成（可以访问当前this实例）
+  created() {
+
+  },
+  // 生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {
+    this.mapEchartsInit()
+  },
+  // 方法集合
+  methods: {
+    mapEchartsInit() {
+      var myChart = echarts.init(this.$refs.myCharts, 'macarons')
+      myChart.setOption(option, true)
+    }
+  }
+}
+</script>
+<style lang='scss' scoped>
+//@import url(); 引入公共css类
+  .chartBox_d {
+    // height:450px;
+    width:100%;
+    height: 100%;
+    // box-sizing: border-box;
+    // padding: 30px 20px 30px 20px;
+  }
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,18 +1,83 @@
 <template>
   <div class="home-container">
-    <div class="home-text">name: {{ name }}</div>
+    <split-pane :min-percent="20" :default-percent="20" split="vertical">
+      <template slot="paneL">
+        <div class="left-container">
+          A
+        </div>
+      </template>
+      <template slot="paneR">
+        <split-pane split="horizontal" :min-percent="20" :default-percent="80">
+          <template slot="paneL">
+            <split-pane split="vertical" :min-percent="20" :default-percent="80">
+              <template slot="paneL">
+                <split-pane split="horizontal" :min-percent="20" :default-percent="20">
+                  <template slot="paneL">
+                    <div class="top-container">
+                      C
+                    </div>
+                  </template>
+                  <template slot="paneR">
+                    <div ref="myCharts" class="center-container">
+                      <!-- <mapVue /> -->
+                    </div>
+                  </template>
+                </split-pane>
+              </template>
+              <template slot="paneR">
+                <div class="right-container">
+                  b1
+                </div>
+              </template>
+            </split-pane>
+          </template>
+          <template slot="paneR">
+            <div class="bottom-container">
+              a
+            </div>
+          </template>
+        </split-pane>
+
+      </template>
+    </split-pane>
+
   </div>
 </template>
 
 <script>
+import echarts from 'echarts'
+import option from './js/mapOption'
+// import 'echarts/map/js/province/guangdong.js'
+import 'echarts/map/js/china.js' // 引入中国地图数据
+require('echarts/theme/macarons')
 import { mapGetters } from 'vuex'
-
+import splitPane from 'vue-splitpane'
 export default {
   name: 'Home',
+  components: {
+    splitPane
+  },
+  data() {
+    return {
+      myChart: null
+    }
+  },
   computed: {
-    ...mapGetters([
-      'name'
-    ])
+    ...mapGetters(['name'])
+  },
+  mounted() {
+    this.mapEchartsInit()
+  },
+  // 方法集合
+  methods: {
+    mapEchartsInit() {
+      var _that = this
+      _that.myChart = echarts.init(this.$refs.myCharts, 'macarons')
+      _that.myChart.setOption(option, true)
+      window.onresize = function() {
+        _that.myChart.resize()
+      }
+    }
   }
 }
 </script>
@@ -20,11 +85,41 @@ export default {
 <style lang="scss" scoped>
 .home {
   &-container {
-    margin: 30px;
+    padding: 10px;
+    position: relative;
+    height: calc(100vh - 50px);
   }
   &-text {
     font-size: 30px;
     line-height: 46px;
   }
 }
+  .left-container {
+    background-color: #F38181;
+    width: 100%;
+    height: 100%;
+  }
+
+  .right-container {
+    background-color: #FCE38A;
+    width: 100%;
+    height: 100%;
+  }
+
+  .top-container {
+    background-color: #fcc18a;
+    width: 100%;
+    height: 100%;
+  }
+
+  .center-container {
+    // background-color: #ac8afc;
+    width: 100%;
+    height: 100%;
+  }
+  .bottom-container {
+    width: 100%;
+    background-color: #95E1D3;
+    height: 100%;
+  }
 </style>
